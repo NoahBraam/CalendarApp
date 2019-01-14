ea62632153e266eb6230344d74c791cd0da08ffa
Noah Braam <nwbraam@gmail.com>
2019-01-14 14:17:29 -0500

    initial print, delete and compare functions

---
+++ assign1/src/CalendarParser.c
@@ -0,0 +1,82 @@
+/**
+ * Noah Braam
+ * 0960202
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "CalendarParser.h"
+#include "LinkedListAPI.h" 
+
+void deleteEvent(void* toBeDeleted) {
+  Event* evt = (Event*)toBeDeleted;
+  deleteDate(evt->creationDateTime);
+  deleteDate(evt->startDateTime);
+  freeList(evt->properties);
+  freeList(evt->alarms);
+  free(evt);
+}
+
+int compareEvents(const void* first, const void* second) {
+  return 1;
+}
+
+char* printEvent(void* toBePrinted) {
+  return "temp";
+}
+
+void deleteAlarm(void* toBeDeleted) {
+  Alarm* alarm = (Alarm*)toBeDeleted;
+  free(alarm->trigger);
+  freeList(alarm->properties);
+  free(alarm);
+}
+
+int compareAlarms(const void* first, const void* second) {
+  return 1;
+}
+
+char* printAlarm(void* toBePrinted) {
+  return "temp";
+}
+
+void deleteProperty(void* toBeDeleted) {
+  Property* prop = (Property*)toBeDeleted;
+  free(prop->propDescr);
+  free(prop);
+}
+
+int compareProperties(const void* first, const void* second) {
+  return 1;
+}
+
+char* printProperty(void* toBePrinted) {
+  return "Temp";
+}
+
+void deleteDate(void* toBeDeleted) {
+  free(toBeDeleted);
+}
+
+int compareDates(const void* first, const void* second) {
+  return 1;
+}
+
+char* printDate(void* toBePrinted) {
+  if (toBePrinted == NULL) {
+    return NULL;
+  }
+  DateTime* dt = (DateTime*)toBePrinted;
+  char* print = malloc(sizeof(char) * 23);
+  strcat(print, dt->date);
+  strcat(print, dt->time);
+
+  if (dt->UTC) {
+    strcat(print, "true");
+  } else {
+    strcat(print, "false");
+  }
+
+  return print;
+}
