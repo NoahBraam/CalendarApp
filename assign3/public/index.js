cc7b477ddb485b92e85a0be13a2bfd6a9683acb6
Noah Braam <nwbraam@gmail.com>
2019-03-19 21:46:19 -0400

    big refactor

--- assign3/public/index.js
+++ assign3/public/index.js
@@ -8,134 +8,10 @@ $(document).ready(function() {
     });
 
     $('#selectFile').on("change", function(event) {
-        $.ajax({
-            type: 'get',
-            dataType: 'json',
-            url: '/getEventList',
-            data: {
-                filename: $('#selectFile').val()
-            },
-            success: function (data) {
-                console.log(data);
-                $("#calendarview td").parent().remove();
-                for (i = 0; i<data.length; i++) {
-                    datStr = data[i].startDT.date;
-                    timeStr = data[i].startDT.time;
-                    if (data[i].startDT.UTC) {
-                        timeStr += " (UTC)";
-                    }
-                    var htmlRow = `<tr><td>${i+1}</td><td>${datStr}</td><td>${timeStr}</td><td>${data[i].summary}</td><td>${data[i].numProps}</td><td>${data[i].numAlarms}</td><td><a name="${i+1}" id="alm${i}" nohref style="cursor:pointer;color:blue;text-decoration:underline">Show Alarms</a><p><a name="${i+1}" id="prop${i}" nohref style="cursor:pointer;color:blue;text-decoration:underline">Show Props</a></td></tr>`;
-                    $('#calendarview tr:last').after(htmlRow);                    
-                }
-                for (j = 0; j < data.length; j++) {
-                    $(`#alm${j}`).on("click", function(e) {
-                        e.preventDefault();
-                        const evtNo = e.currentTarget.name;
-                        $.ajax({
-                            type: 'get',
-                            dataType: 'json',
-                            url: '/getAlarmJSON',
-                            data: {
-                                filename: $('#selectFile').val(),
-                                evtNumber: evtNo
-                            },
-                            success: function (dt) {
-                                if (dt.length !== 0) {
-                                    var textToAppend = "";
-                                    for (i = 0; i<dt.length; i++) {
-                                        textToAppend += `\r\nAlarm ${i+1}) Trigger: ${dt[i].trigger}    Action: ${dt[i].action}\r\n`;
-                                        if (dt[i].properties.length !== 0) {
-                                            textToAppend+=`Additional Properties: `;
-                                            for (j = 0; j<dt[i].properties.length; j++) {
-                                                textToAppend+=`\r\nProperty Name: ${dt[i].properties[j].name}     Property Description: ${dt[i].properties[j].descr}`
-                                            }
-                                        }
-                                    }
-                                    $("#statuspanel").append(textToAppend + "\r\n");
-                                    scroll();
-                                }
-                            },
-                            fail: function (error) {
-                                // Non-200 return, do something with error
-                                console.log(error);
-                            }
-                        });
-                    });
-                    $(`#prop${j}`).on("click", function(e) {
-                        e.preventDefault();
-                        const evtNo = e.currentTarget.name;
-                        $.ajax({
-                            type: 'get',
-                            dataType: 'json',
-                            url: '/getOptionalProps',
-                            data: {
-                                filename: $('#selectFile').val(),
-                                evtNumber: evtNo
-                            },
-                            success: function (dt) {
-                                if (dt.length !== 0) {
-                                    var textToAdd =`\r\nAdditional Properties: `;
-                                    for (i = 0; i<dt.length; i++) {
-                                        textToAdd+=`\r\nProperty Name: ${dt[i].name}     Property Description: ${dt[i].descr}`
-                                    }
-                                    $("#statuspanel").append(textToAdd + "\r\n");
-                                    scroll();
-                                }
-                            },
-                            fail: function (error) {
-                                // Non-200 return, do something with error
-                                console.log(error);
-                            }
-                        });
-                    });
-                }
-
-            },
-            fail: function (error) {
-                // Non-200 return, do something with error
-                console.log(error);
-            }
-        });
+        updateCalView();
     });
 
-    $.ajax({
-        type: 'get',
-        dataType: 'json',
-        url: '/filenames',
-        success: function (data) {
-            if (data.numFiles === 0) {
-                $('#filelog tr:last').after(`<tr><td>No Files on Server</td></tr>`);
-            } else {
-                for (i = 0; i<data.numFiles; i++) {
-                    if (data.files[i].endsWith(".ics")) {
-                        $.ajax({
-                            type: 'get',
-                            dataType: 'json',
-                            url: '/parsefileReturnCal',
-                            data: {
-                                filename : data.files[i]
-                            },
-                            success: function(resp) {
-                                console.log(resp);
-                                $("#selectFile").append(new Option(resp.filename, ""+resp.filename+""));
-                                $("#createFileSelect").append(new Option(resp.filename, ""+resp.filename+""));
-                                var htmlRow = `<tr><td><a href = "/uploads/${resp.filename}">${resp.filename}</a></td><td>${resp.version}</td><td>${resp.prodID}</td><td>${resp.numEvents}</td><td>${resp.numProps}</td></tr>`;
-                                $('#filelog tr:last').after(htmlRow);
-                            },
-                            fail: function(error) {
-                                // Non-200 return, do something with error
-                                console.log(error);
-                            }
-                        });
-                    }
-                }
-            }
-        },
-        fail: function(error) {
-            // Non-200 return, do something with error
-            console.log(error); 
-        }
-    });
+    updateFileList();
 
     $("#createFileSelect").on("change", function (event) {
         var val = $("#createFileSelect").val();
@@ -192,6 +68,8 @@ $(document).ready(function() {
                     // Refresh view components
                     $("#statuspanel").append(`Created new file: ${json.filename}\r\n`);
                     scroll();
+                    updateFileList();
+                    updateCalView();
                 },
                 fail: function(error) {
 
@@ -216,6 +94,8 @@ $(document).ready(function() {
                     // Refresh view components
                     $("#statuspanel").append(`Created new event in ${json.filename}\r\n`);
                     scroll();
+                    updateFileList();
+                    updateCalView();
                 },
                 fail: function(error) {
 
@@ -239,4 +119,145 @@ $(document).ready(function() {
         if(stat.length)
             stat.scrollTop(stat[0].scrollHeight - stat.height());
     }
+
 });
+
+function updateFileList() {
+    $.ajax({
+        type: 'get',
+        dataType: 'json',
+        url: '/filenames',
+        success: function (data) {
+            if (data.numFiles === 0) {
+                $('#filelog tr:last').after(`<tr><td>No Files on Server</td></tr>`);
+            } else {
+                $("#filelog").find("tr:gt(0)").remove();
+                var select = document.getElementById("selectFile");
+                var select2 = document.getElementById("createFileSelect");
+                var length = select.options.length;
+                for (i = 1; i < length; i++) {
+                    select.options[i] = null;
+                    select2.options[i] = null;
+                }
+                for (i = 0; i<data.numFiles; i++) {
+                    if (data.files[i].endsWith(".ics")) {
+                        $.ajax({
+                            type: 'get',
+                            dataType: 'json',
+                            url: '/parsefileReturnCal',
+                            data: {
+                                filename : data.files[i]
+                            },
+                            success: function(resp) {
+                                console.log(resp);
+                                $("#selectFile").append(new Option(resp.filename, ""+resp.filename+""));
+                                $("#createFileSelect").append(new Option(resp.filename, ""+resp.filename+""));
+                                var htmlRow = `<tr><td><a href = "/uploads/${resp.filename}">${resp.filename}</a></td><td>${resp.version}</td><td>${resp.prodID}</td><td>${resp.numEvents}</td><td>${resp.numProps}</td></tr>`;
+                                $('#filelog tr:last').after(htmlRow);
+                            },
+                            fail: function(error) {
+                                // Non-200 return, do something with error
+                                console.log(error);
+                            }
+                        });
+                    }
+                }
+            }
+        },
+        fail: function(error) {
+            // Non-200 return, do something with error
+            console.log(error); 
+        }
+    });
+}
+
+function updateCalView() {
+    $.ajax({
+        type: 'get',
+        dataType: 'json',
+        url: '/getEventList',
+        data: {
+            filename: $('#selectFile').val()
+        },
+        success: function (data) {
+            console.log(data);
+            $("#calendarview").find("tr:gt(0)").remove();
+            for (i = 0; i<data.length; i++) {
+                datStr = data[i].startDT.date;
+                timeStr = data[i].startDT.time;
+                if (data[i].startDT.UTC) {
+                    timeStr += " (UTC)";
+                }
+                var htmlRow = `<tr><td>${i+1}</td><td>${datStr}</td><td>${timeStr}</td><td>${data[i].summary}</td><td>${data[i].numProps}</td><td>${data[i].numAlarms}</td><td><a name="${i+1}" id="alm${i}" nohref style="cursor:pointer;color:blue;text-decoration:underline">Show Alarms</a><p><a name="${i+1}" id="prop${i}" nohref style="cursor:pointer;color:blue;text-decoration:underline">Show Props</a></td></tr>`;
+                $('#calendarview tr:last').after(htmlRow);                    
+            }
+            for (j = 0; j < data.length; j++) {
+                $(`#alm${j}`).on("click", function(e) {
+                    e.preventDefault();
+                    const evtNo = e.currentTarget.name;
+                    $.ajax({
+                        type: 'get',
+                        dataType: 'json',
+                        url: '/getAlarmJSON',
+                        data: {
+                            filename: $('#selectFile').val(),
+                            evtNumber: evtNo
+                        },
+                        success: function (dt) {
+                            if (dt.length !== 0) {
+                                var textToAppend = "";
+                                for (i = 0; i<dt.length; i++) {
+                                    textToAppend += `\r\nAlarm ${i+1}) Trigger: ${dt[i].trigger}    Action: ${dt[i].action}\r\n`;
+                                    if (dt[i].properties.length !== 0) {
+                                        textToAppend+=`Additional Properties: `;
+                                        for (j = 0; j<dt[i].properties.length; j++) {
+                                            textToAppend+=`\r\nProperty Name: ${dt[i].properties[j].name}     Property Description: ${dt[i].properties[j].descr}`
+                                        }
+                                    }
+                                }
+                                $("#statuspanel").append(textToAppend + "\r\n");
+                                scroll();
+                            }
+                        },
+                        fail: function (error) {
+                            // Non-200 return, do something with error
+                            console.log(error);
+                        }
+                    });
+                });
+                $(`#prop${j}`).on("click", function(e) {
+                    e.preventDefault();
+                    const evtNo = e.currentTarget.name;
+                    $.ajax({
+                        type: 'get',
+                        dataType: 'json',
+                        url: '/getOptionalProps',
+                        data: {
+                            filename: $('#selectFile').val(),
+                            evtNumber: evtNo
+                        },
+                        success: function (dt) {
+                            if (dt.length !== 0) {
+                                var textToAdd =`\r\nAdditional Properties: `;
+                                for (i = 0; i<dt.length; i++) {
+                                    textToAdd+=`\r\nProperty Name: ${dt[i].name}     Property Description: ${dt[i].descr}`
+                                }
+                                $("#statuspanel").append(textToAdd + "\r\n");
+                                scroll();
+                            }
+                        },
+                        fail: function (error) {
+                            // Non-200 return, do something with error
+                            console.log(error);
+                        }
+                    });
+                });
+            }
+
+        },
+        fail: function (error) {
+            // Non-200 return, do something with error
+            console.log(error);
+        }
+    });
+}
